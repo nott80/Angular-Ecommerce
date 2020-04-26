3fb60723e6207a49ced03e2b7bc44fcb2725340f
Tolong App <dev.tolongtech@gmail.com>
2020-04-26 20:33:45 +0800

    Added display logic

---
+++ src/products.ts
@@ -0,0 +1,19 @@
+export const websites = [
+
+{
+  name: 'Swift Media Solution',
+  link: 'https://swiftmediasolution.com',
+  specialize: 'Angular'
+},
+{
+  name: 'Bingo Web Solution',
+  link: 'https://bingowebsolution.com',
+  specialize: 'Search Engine Optimization'
+},
+{
+  name: 'Zetlink Web',
+  link: 'https://zelinkweb.com',
+  specialize: 'Website development'
+}
+
+];
